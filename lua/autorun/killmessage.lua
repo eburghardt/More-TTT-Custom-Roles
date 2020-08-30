b79ab920e2b14b84f7cbe3f5c7144bc8272be82d
arich00 <rich32@purdue.edu>
2020-08-29 23:26:30 -0400

    added cursed to files in LUA folder

--- lua/autorun/killmessage.lua
+++ lua/autorun/killmessage.lua
@@ -16,6 +16,7 @@ if (CLIENT) then
 		local AssassinColor = Color(112, 50, 0)
 		local KillerColor = Color(50, 0, 70)
 		local DoctorColor = Color(7, 183, 160)
+		local CursedColor = Color(2, 37, 69)
 		
 		local NameColor = Color(142, 68, 173)
 		local UnknownColor = Color(152, 48, 196)
@@ -59,6 +60,9 @@ if (CLIENT) then
 		elseif role == ROLE_VAMPIRE then
 			col = VampireColor
 			role = "a vampire"
+		elseif role == ROLE_CURSED then
+			col = CursedColor
+			role = "a cursed"
 		elseif role == ROLE_SWAPPER then
 			col = SwapperColor
 			role = "a swapper"
